4ca26f54fc638b325d227b1c0c6fa295f2723144
Nikolay Nikolaev <evrinoma@gmail.com>
2020-01-20 16:01:02 +0300

    init commit

---
+++ srv.js
@@ -0,0 +1,65 @@
+const chokidar = require('chokidar');
+const fs = require('fs-extra');
+const pathBase = '/opt/WWW/kis.nekeng.ru/content/1c/'
+const pathTracking = pathBase + 'out/'
+const pathTarget = pathBase + 'clone/'
+const lenBase = pathBase.length
+
+const watcher = chokidar.watch(pathTracking, {
+    ignored: /(^|[\/\\])\../, // ignore dotfiles
+    persistent: true
+});
+
+var log = console.log.bind(console);
+
+function createDir(path)
+{
+	if (!fs.existsSync(path)) {
+		fs.mkdirSync(path);
+	}
+}
+
+createDir(pathTarget);
+
+watcher
+.on('add',		(path) =>  {
+	console.log('File', path, 'has been changed File [', pathTarget + path.substr(lenBase), ']');
+	fs.copy(path, pathTarget + path.substr(lenBase), function(err){
+		if (err) return console.error(err);
+		console.log("success!")
+	});
+})
+.on('addDir',		(path) =>  {
+	console.log('Directory', path, 'has been added Dir [', pathTarget + path.substr(lenBase), ']');
+	createDir(pathTarget + path.substr(lenBase));
+})
+.on('change',		(path) =>  {
+	console.log('File', path, 'has been changed File [', pathTarget + path.substr(lenBase), ']');
+	fs.copy(path, pathTarget + path.substr(lenBase), function(err){
+		if (err) return console.error(err);
+		console.log("success!")
+	});
+})
+.on('unlink',		(path) =>  {
+	console.log('File', path, 'has been removed File [', pathTarget + path.substr(lenBase), ']');
+	
+	fs.stat(pathTarget + path.substr(lenBase), function(err, stat) {
+		if(err == null) {
+			console.log('File exists');
+			fs.unlink(pathTarget + path.substr(lenBase), err => {
+				console.error(err)
+			})
+		}
+	});
+})
+.on('unlinkDir',	(path) =>  {
+	console.log('Directory', path, 'has been removed Dir [', pathTarget + path.substr(lenBase), ']');
+	if (fs.existsSync(pathTarget + path.substr(lenBase))) {
+		fs.remove(pathTarget + path.substr(lenBase), err => {
+			console.error(err)
+		})
+	}
+})
+.on('error',		(error) => {
+	console.error('Error happened', error);
+})
